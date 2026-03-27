16ea63dd8cb8a37589397becb875db1da85eb72a
Almassi101 <almassi101@outlook.com>
2026-03-27 11:13:49 +0100

    Fix formatting issues in deur.php

--- deur.php
+++ deur.php
@@ -12,28 +12,28 @@ class Deur {
     }
 
     public function sleutelGebruiken(string $richting): string { 
-        if ($richting == "links") { // check they key if turned left
+        if ($richting == "links") { // check the key if turned left
             $this->opSlot = false; // unlock the door
-            return "Deur is ontgrendeld"; // return message
+            return "Deur is ontgrendeld"; 
         } else { 
             $this->opSlot = true; // keep the door locked
-            return $this->deurNaam . " is nog op slot"; // return message
+            return $this->deurNaam . " is nog op slot"; 
         }
     }
 
-    public function deurOpenen(): string { // methode to open the door 
+    public function deurOpenen(): string { 
         if ($this->opSlot) { // check if the door is locked
-            return "Deur is nog op slot en kan niet open"; // return message if the door is locked
+            return "Deur is nog op slot en kan niet open"; 
         }
         $this->deuropening = true; // set the door opening to true
-        return "Deur is geopend!"; // return message if the door is opened
+        return "Deur is geopend!"; 
     }
 
-    public function doorDeurLopen(): string { //methode to walk through the door
+    public function doorDeurLopen(): string { 
         if (!$this->opSlot && $this->deuropening) { // check if the door is unlocked and open
-            return "Je bent in je huiskamer!"; // return message if the door is unlocked and open
+            return "Je bent in je huiskamer!"; 
         } else {
-            return "Je stoot tegen de " . $this->deurNaam . "!"; // return message if the door is locked or closed
+            return "Je stoot tegen de " . $this->deurNaam . "!"; 
         }
     }
 }
